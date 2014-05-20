62bc55a829eec93bae635ceebd09ce854815eb67
Kenan Yildirim <kenan@kenany.me>
2014-05-20 14:17:17 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# nwitch-movie
+
+nwitch plugin for looking up movies through the
+[OMDb API](http://www.omdbapi.com/).
+
+## Installation
+
+``` bash
+$ npm install --save nwitch-movie
+```
+
+## Usage
+
+``` toml
+[plugins]
+nwitch-movie = true
+```
+
+``` irc
+05:33 <KenanY> !movie The Room
+05:34 <nwitch> KenanY: The Room (2003)
+05:34 <nwitch> KenanY: | 3.5/10
+05:34 <nwitch> KenanY: | Drama
+05:34 <nwitch> KenanY: | http://imdb.com/title/tt0368226
+```
---
+++ index.js
@@ -0,0 +1,40 @@
+var behest = require('behest');
+var omdb = require('omdb');
+
+function plugin() {
+  return function(irc) {
+    irc.on('message', function(evt) {
+      var from = evt.from;
+      var to = evt.to;
+      var message = evt.message;
+
+      if (!behest.isValid(message)) {
+        return;
+      }
+
+      var command = behest(message);
+      if (command.command === 'movie') {
+        var destination = to.charAt(0) === '#' ? to : from;
+
+        omdb.get(command.params.join(' '), function(error, movie) {
+          if (error) {
+            irc.send(destination, 'Error looking up movie.');
+            return;
+          }
+
+          if (!movie) {
+            irc.send(destination, 'Movie not found.');
+            return;
+          }
+
+          irc.send(destination, movie.title + ' (' + movie.year + ')');
+          irc.send(destination, '| ' + movie.imdb.rating);
+          irc.send(destination, '| ' + movie.genres.join(', '));
+          irc.send(destination, '| http://imdb.com/title/' + movie.imdb.id);
+        });
+      }
+    });
+  };
+}
+
+module.exports = plugin;
